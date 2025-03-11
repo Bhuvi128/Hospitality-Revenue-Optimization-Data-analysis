5200f47498cc201387eba6848de9c0ee85241d7c
Bhuvi128 <bhuvanendiran128@gmail.com>
2025-03-11 15:28:12 +0530

    update

--- Hospitality Revenue Analysis.sql
+++ Hospitality Revenue Analysis.sql
@@ -1,3 +1,15 @@
+/* 1. Retreive total property for each property category */
+
+select category, count(*) as tot_cust from dim_hotels
+group by category;
+
+
+/* 1. Retreive the data for total no of guests for each property category*/
+
+select dh.category, sum(fb.no_guests)
+from fact_bookings fb left join dim_hotels dh
+on fb.property_id = dh.property_id
+group by dh.category;
 
 
 
