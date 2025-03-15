52bffbfcd1fc4e5b10c44ec65fd267fc57e5f354
Bhuvi128 <bhuvanendiran128@gmail.com>
2025-03-15 10:31:11 +0530

    update

--- Hospitality Revenue Analysis.sql
+++ Hospitality Revenue Analysis.sql
@@ -10,3 +10,18 @@ group by booking_status;
 The average of revenue generated and revenue realized is equal for both no show and checked out,
 but for cancelled bookings there is significant difference */
 
+/* 2. Retreive average revenue generated and revenue realized by room category */
+
+select dr.room_class, round(avg(fb.revenue_generated),2) avg_revenue_generated, 
+round(avg(fb.revenue_realized),2) avg_revenue_realized
+from fact_bookings fb left join dim_rooms dr
+on fb.room_category = dr.room_id
+group by dr.room_class;
+
+/* Insights:
+There is a significant difference for average revenue generated and average revenue realized
+for all room class, indicating in all room class there may be cancelled bookings so there would 
+be fincancial impact in this pattern.
+*/
+
+select * from dim_rooms;
