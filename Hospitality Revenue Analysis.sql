ffecc39d541a1d01909807e645904f08810d6152
Bhuvi128 <bhuvanendiran128@gmail.com>
2025-03-17 11:05:28 +0530

    update

--- Hospitality Revenue Analysis.sql
+++ Hospitality Revenue Analysis.sql
@@ -1,92 +1,86 @@
 use atliq_hospitality_db;
 
-/* 1. Retreive average revenue generated and revenue realized by booking_status */
+/* Data Preparation/cleaning */
 
-select booking_status, round(avg(revenue_generated),2) avg_rev_generated, round(avg(revenue_realized),2) avg_rev_realized
-from fact_bookings
-group by booking_status;
+/* 1. Check for duplicates */
 
-/* Insights:
-The average of revenue generated and revenue realized is equal for both no show and checked out,
-but for cancelled bookings there is significant difference */
+-- Lets check duplicates for fact bookings table
 
-/* 2. Retreive average revenue generated and revenue realized by room category */
+select * from
+(select *, count(*) over(partition by booking_id) as dup_count 
+from fact_bookings) dup_check
+where dup_count > 1;  -- using window function, extracted duplicate entries in fact bookings table. There is no duplicates.
 
-select dr.room_class, round(avg(fb.revenue_generated),2) avg_revenue_generated, 
-round(avg(fb.revenue_realized),2) avg_revenue_realized
+-- Lets check duplicates for fact aggregated bookings table
+
+select * from
+(select *, count(*) over(partition by property_id) as dup_count
+from fact_aggregated_bookings) dup_check
+where dup_count > 1; -- There is duplicates in fact aggregated bookings table, but every duplicates have different features.
+
+-- Lets check duplicates for dim hotels table
+
+select * from
+(select *, count(*) over(partition by property_id) dup_count
+from dim_hotels) dup_check
+where dup_count > 1; -- There is no duplicate entries based on property_id in dim hotels table
+
+-- Lets check duplicates for dim rooms table
+
+select * from
+(select *, count(*) over(partition by room_id) dup_count
+from dim_rooms) dup_check
+where dup_count > 1; -- No duplicates
+
+-- Lets check duplicates for dim date table
+
+select * from
+(select *, count(*) over(partition by date_d) dup_count
+from dim_date) dup_check
+where dup_count > 1; -- No duplicates
+
+/* 2. Check for data types */
+
+-- Lets check for fact bookings table
+describe fact_bookings; -- No invalid data types
+
+-- Lets check for fact aggregated bookings
+describe fact_aggregated_bookings; -- Data types in fact aggregated bookings table are valid;
+
+-- Lets check for dim hotels table
+describe dim_hotels; -- Data types are valid in dim hotels table
+
+-- Lets check for dim rooms table
+describe dim_rooms; -- Valid data types
+
+-- Lets check for dim date table
+describe dim_date; -- valid data types
+
+/* Data analysis */
+
+/* 1. Retreive total bookings from fact_bookings table */
+
+select count(*) total_bookings  -- Extracted total bookings using count of bookings from fact bookings table
+from fact_bookings;
+
+/* 2. Retreive revenue generated and realized by booking status */
+
+select booking_status, revenue_generated, revenue_realized
+from fact_bookings;  -- Extracted revenue generated and realized by booking_status
+
+/* 3. Retreive revenue generated and realized by room class */
+
+select dr.room_class, fb.revenue_generated, fb.revenue_realized
 from fact_bookings fb left join dim_rooms dr
+on fb.room_category = dr.room_id;  -- Retreived revenue generated and realized for each room class by joining dim rooms table and fact bookings table
+
+/* 4. Retreive revenue generated and realized by room class for cancelled bookings */
+
+select dr.room_class, booking_status, revenue_generated, revenue_realized
+from
+(select * from fact_bookings
+where booking_status = 'Cancelled') fb left join dim_rooms dr
 on fb.room_category = dr.room_id
-group by dr.room_class;
-
-/* Insights:
-There is a significant difference for average revenue generated and average revenue realized
-for all room class, indicating in all room class there may be cancelled bookings so there would 
-be fincancial impact in this pattern.
-*/
-
-/* 3. Analyze which room class has higher successfull and cancelled bookings */
-
-select dr.room_class, count(*) total_bookings, 
-sum(case when fb.booking_status in ('Checked Out', 'No Show') then 1 else 0 end) successfull_bookings,
-sum(case when fb.booking_status = 'Cancelled' then 1 else 0 end) cancelled_bookings
-from fact_bookings fb left join dim_rooms dr
-on fb.room_category = dr.room_id
-group by dr.room_class;
-
-/* Insights:
-The Elite room class has higher successfull bookings and cancelled bookings.
-The reason behind Elite room class higher in both successfull and cancelled bookings is because
-of total bookings in Elite room class is higher than other room class. This may happen 
-because more rooms booked there is likely to be more cancellations. */
-
-/* 4. Analyze which room class has higher cancellation rate */
-
-with room_bookings as (
-select dr.room_class, count(*) total_bookings, 
-sum(case when fb.booking_status = 'Cancelled' then 1 else 0 end) cancelled_bookings
-from fact_bookings fb left join dim_rooms dr
-on fb.room_category = dr.room_id
-group by dr.room_class
-)
-select room_class, round((cancelled_bookings/total_bookings) * 100,2) cancellation_rate
-from room_bookings;
-
-/* Insights:
-The cancellation rate for all room class are 24% to 25% approximately,
-indicating there is no revenue loss pattern for specific room class.
-So the revenue loss pattern may be in any other categories like
-luxury or high cost Properties. Investigating other categories may reveal 
-actionable insights. */
-
-select * from fact_bookings;
-select * from dim_hotels;
-
-/* 5. Retreive average revenue generated and realized for each Property category */
-
-select dh.category, round(avg(fb.revenue_generated),2) avg_revenue_generated, 
-round(avg(fb.revenue_realized),2) avg_revenue_realized
-from fact_bookings fb left join dim_hotels dh
-on fb.property_id = dh.property_id
-group by dh.category;
-
-/* Insights:
-The average revenue generated for Business Property is higher than Luxury Property,
-indicating more bookings are in Business Properties. The difference of average 
-between revenue generated and revenue realized suggesting there may be revenue 
-loss due to cancelled bookings. So Investigating these may give actionable insights.
-*/
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
